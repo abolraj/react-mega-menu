4c55707b3b2193fddf8fd5fa9853849f9dea7f10
abolraj <abolfazlrajaeenasab@gmail.com>
2024-12-14 19:10:59 +0330

    feat - Add MegaMenu component

---
+++ src/MegaMenu.jsx
@@ -0,0 +1,102 @@
+import "./MegaMenu.css";
+
+export default function(){
+    return (
+        <>
+            <div class="header-container">
+                <nav class="header-nav">
+                    <ul>
+                        <li class="menu-item">
+                            Mobile
+                            <ul class="submenu-list">
+                                <li className="submenu-item">
+                                    Apple
+                                    <ul className="sub2menu-list">
+                                        <li className="sub2menu-item">Apple 1</li>
+                                        <li className="sub2menu-item">Apple 2</li>
+                                        <li className="sub2menu-item">Apple 3</li>
+                                        <li className="sub2menu-item">Apple 4</li>
+                                    </ul>
+                                </li>
+                                <li className="submenu-item">
+                                    Samsung
+                                    <ul className="sub2menu-list">
+                                        <li className="sub2menu-item">Samsung 1</li>
+                                        <li className="sub2menu-item">Samsung 2</li>
+                                        <li className="sub2menu-item">Samsung 3</li>
+                                        <li className="sub2menu-item">Samsung 4</li>
+                                    </ul>
+                                </li>
+                                <li className="submenu-item">
+                                    Mi
+                                    <ul className="sub2menu-list">
+                                        <li className="sub2menu-item">Mi 1</li>
+                                        <li className="sub2menu-item">Mi 2</li>
+                                        <li className="sub2menu-item">Mi 3</li>
+                                        <li className="sub2menu-item">Mi 4</li>
+                                    </ul>
+                                </li>
+                                <li className="submenu-item">
+                                    Huawei
+                                    <ul className="sub2menu-list">
+                                        <li className="sub2menu-item">Huawei 1</li>
+                                        <li className="sub2menu-item">Huawei 2</li>
+                                        <li className="sub2menu-item">Huawei 3</li>
+                                        <li className="sub2menu-item">Huawei 4</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item">
+                            TV
+                            <ul class="submenu-list">
+                                <li className="submenu-item">
+                                    Apple
+                                    <ul className="sub2menu-list">
+                                        <li className="sub2menu-item">Apple 1</li>
+                                        <li className="sub2menu-item">Apple 2</li>
+                                        <li className="sub2menu-item">Apple 3</li>
+                                        <li className="sub2menu-item">Apple 4</li>
+                                    </ul>
+                                </li>
+                                <li className="submenu-item">
+                                    Samsung
+                                    <ul className="sub2menu-list">
+                                        <li className="sub2menu-item">Apple 1</li>
+                                        <li className="sub2menu-item">Apple 2</li>
+                                        <li className="sub2menu-item">Apple 3</li>
+                                        <li className="sub2menu-item">Apple 4</li>
+                                    </ul>
+                                </li>
+                                <li className="submenu-item">
+                                    Xiaomi
+                                    <ul className="sub2menu-list">
+                                        <li className="sub2menu-item">Apple 1</li>
+                                        <li className="sub2menu-item">Apple 2</li>
+                                        <li className="sub2menu-item">Apple 3</li>
+                                        <li className="sub2menu-item">Apple 4</li>
+                                    </ul>
+                                </li>
+                                <li className="submenu-item">
+                                    LG
+                                    <ul className="sub2menu-list">
+                                        <li className="sub2menu-item">Apple 1</li>
+                                        <li className="sub2menu-item">Apple 2</li>
+                                        <li className="sub2menu-item">Apple 3</li>
+                                        <li className="sub2menu-item">Apple 4</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item">
+                            Fridge
+                        </li>
+                        <li class="menu-item">
+                            Gift
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
